2036d724e41a5f40e195e67b0d3a1dcff860f5e7
Vladislav Afanasiev <afanasev098@gmail.com>
2025-10-06 13:46:39 +0000

    puthon

---
+++ pyth2.py
@@ -0,0 +1,3 @@
+x=eval(input('enter number'))
+y=eval(input('enter number'))
+print("result", (x)+(y))
---
+++ pyth3.py
@@ -0,0 +1,3 @@
+find = "try to find me if you can find me"
+finder = find.rfind("find")
+print(finder)
